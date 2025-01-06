546555ec0533fdffd73fc48bdceebdced5c514de
sachinsamala <2211cs010512@mallareddyuniversity.ac.in>
2025-01-06 11:54:12 +0530

    pushing codes

---
+++ demo.js
@@ -0,0 +1,44 @@
+var a = 10; //global
+
+let b = 20; // script
+const c=30; // script
+
+function demo(){
+    var username = "Mahesh";
+    let city = "Hyd";
+    const salary = 1234567890;
+    console.log(username , city , salary);
+}
+demo();
+
+{
+    let n = 1000;
+    const q = 2000;
+    console.log(n , q);
+}
+
+let str1= "double quotes";
+console.log(str1 , typeof(str1));
+
+ let str2= 'single quote';
+ console.log(str2 , typeof(str2));
+
+ let bool1= true;
+ console.log(bool1 , typeof(bool1));
+
+ let bool2= false;
+ console.log(bool2 , typeof(bool2));
+
+ 
+
+ let num1 = 10;
+ console.log(num1 , typeof(num1));
+
+ let num2 = 10.2345;
+ console.log(num2 , typeof(num2));
+
+ let num3 = 10e3;
+ console.log(num2 , typeof(num3));
+
+ let x = null;
+ console.log(x , typeof(x));

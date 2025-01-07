3e3303942fed9153c7e26f465dcf8147112c43aa
sachinsamala <2211cs010512@mallareddyuniversity.ac.in>
2025-01-07 14:11:27 +0530

    js

---
+++ conditions.js
@@ -0,0 +1,28 @@
+let isAdult = 20;
+if(isAdult >= 18){
+    console.log("he is major");
+}else{
+    console.log("he is minor");
+}
+
+//elseif
+
+let days = prompt("Enter a day");
+if(days == 1){
+    console.log("sunday");
+}
+else if(days == 2){
+    console.log("mon");
+}else if (days == 3){
+    console.log("tues");
+}else if (days == 4){
+    console.log("wed");
+}else if (days == 5){
+    console.log("thurs");
+}else{
+    console.log("invalid day");
+}
+
+//switch
+let day = prompt("Enter a day");
+
---
+++ operators.js
@@ -0,0 +1,63 @@
+//addition
+console.log(5+5);
+console.log(100+1000);
+console.log(10+"100");//concatenation happens because it is in string form
+console.log(10+20+"hi");
+console.log("hi"+10+50);
+
+//subtraction
+console.log(10-5);
+console.log("10"-5);//typecasting - changing of one data type to another datatype 
+console.log("10"-"2");
+
+//multiplication
+console.log(5*5);
+console.log(5*"5");//typecasting
+
+//division
+console.log(10/5);
+console.log("10"/5);//typecasting
+
+//power
+console.log(2**5);
+console.log(5**2);
+console.log("5"**"5");//typecasting
+
+//modulus
+console.log(10%2);
+console.log("10"%5);//typecasting
+
+//increment
+let a = 10;
+console.log(a);
+console.log(++a);//preincrement
+console.log(a++);//postincrement
+
+//decrement
+console.log(--a);//predecrement
+console.log(a--);//postdecrement
+console.log(a);
+
+//assignmentoperators
+
+let b = 10;
+console.log(a+5);
+console.log(a+=5);
+console.log(a-=2);
+console.log(a*=3);
+console.log(a%=5);
+
+//relational operators
+
+console.log(10==10);
+console.log("10" == 10);
+console.log("hi" == "hi");
+console.log("10" === "10");
+console.log("10" === 10);
+console.log(10 != 10);
+console.log("10" !== 10);
+
+//logical operators
+
+
+

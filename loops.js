c824189b25172a2e2883100637bccb1a40f97072
sachinsamala <2211cs010512@mallareddyuniversity.ac.in>
2025-01-08 13:18:15 +0530

    8/1/25

---
+++ loops.js
@@ -0,0 +1,43 @@
+//for (let i=1; i<=5;i++){
+ //   console.log("java script");
+//}
+
+//for (let i=1;i<=100;i++){
+ //   console.log("Java script");
+//}
+
+//for (let i=1; i<=100; i++){
+  //  if(i%2 !=0){
+    //    console.log(i,"is even");
+    
+//}else{
+ //   console.log(i,"is odd");
+//}
+//}
+
+//WAJP to finf the factorial of a number
+// 5 =120; 5*4*3*4*2*1
+
+//let mul=1;
+//let num = Number(prompt("enter a number"));
+//for (int i=1;i<=num;i++){
+  //  mul = mul*i;
+//}
+//console.log(mul);
+
+//let i =1;
+//while(i>=5;)
+//{
+    //console.log("hello");
+  //  i++;
+//}
+//let i=1;
+//do{
+  //  i++;
+    //console.log("hello");
+//}while(i<=5);
+
+function addtocart(){
+    alert("product is added");
+    document.body.style.background="red";
+}

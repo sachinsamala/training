ebbcf83d9dc5d39ea04184376b2b8efa8ad442c4
sachinsamala <2211cs010512@mallareddyuniversity.ac.in>
2025-01-09 10:21:53 +0530

    9/1/25

---
+++ function.js
@@ -0,0 +1,61 @@
+// // // // function addtocart(){
+// // // //     alert("product is added to cart successfully");
+// // // //     document.body.style.background="GREY";
+// // // // }
+// // // // addtocart();
+
+// // // // function demo(){
+// // // //     console.log("Hello World");
+// // // //     document.body.style.background="GREY";
+// // // //     document.body.style..color="red";
+// // // // }
+
+// // // // demo();
+
+// // // function add(a,b){
+// // //     console.log(a);
+// // //     console.log(b);
+// // // }
+// // // add(5,6);
+
+// // function(){
+
+
+// // }
+
+// var dermo = function(){
+//     console.log("anonymous function executing");
+// }
+// demo();
+
+// function test(){
+//     console.log("this is a test function");
+// }
+// test();
+
+
+// (function(){
+//     console.log("IIFE function executing");
+    
+// })
+// ();
+
+//arrow function
+
+// let x = ()=>{
+//     console.log("Arrow function executing");
+ 
+// };
+// x();
+
+//function test1(a,b){
+//     return a+b;
+// }
+// console.log(test1(5,5));
+
+// let x = (a,b) => {return a*b};
+// console.log(x(5,5));
+
+// let y = (a,b) => a+b;
+// console.log(y(100,200));
+
--- operators.js
+++ operators.js
@@ -1,63 +1,63 @@
-//addition
-console.log(5+5);
-console.log(100+1000);
-console.log(10+"100");//concatenation happens because it is in string form
-console.log(10+20+"hi");
-console.log("hi"+10+50);
+// //addition
+// console.log(5+5);
+// console.log(100+1000);
+// console.log(10+"100");//concatenation happens because it is in string form
+// console.log(10+20+"hi");
+// console.log("hi"+10+50);
 
-//subtraction
-console.log(10-5);
-console.log("10"-5);//typecasting - changing of one data type to another datatype 
-console.log("10"-"2");
+// //subtraction
+// console.log(10-5);
+// console.log("10"-5);//typecasting - changing of one data type to another datatype 
+// console.log("10"-"2");
 
-//multiplication
-console.log(5*5);
-console.log(5*"5");//typecasting
+// //multiplication
+// console.log(5*5);
+// console.log(5*"5");//typecasting
 
-//division
-console.log(10/5);
-console.log("10"/5);//typecasting
+// //division
+// console.log(10/5);
+// console.log("10"/5);//typecasting
 
-//power
-console.log(2**5);
-console.log(5**2);
-console.log("5"**"5");//typecasting
+// //power
+// console.log(2**5);
+// console.log(5**2);
+// console.log("5"**"5");//typecasting
 
-//modulus
-console.log(10%2);
-console.log("10"%5);//typecasting
+// //modulus
+// console.log(10%2);
+// console.log("10"%5);//typecasting
 
-//increment
-let a = 10;
-console.log(a);
-console.log(++a);//preincrement
-console.log(a++);//postincrement
+// //increment
+// let a = 10;
+// console.log(a);
+// console.log(++a);//preincrement
+// console.log(a++);//postincrement
 
-//decrement
-console.log(--a);//predecrement
-console.log(a--);//postdecrement
-console.log(a);
+// //decrement
+// console.log(--a);//predecrement
+// console.log(a--);//postdecrement
+// console.log(a);
 
-//assignmentoperators
+// //assignmentoperators
 
-let b = 10;
-console.log(a+5);
-console.log(a+=5);
-console.log(a-=2);
-console.log(a*=3);
-console.log(a%=5);
+// let b = 10;
+// console.log(a+5);
+// console.log(a+=5);
+// console.log(a-=2);
+// console.log(a*=3);
+// console.log(a%=5);
 
-//relational operators
+// //relational operators
 
-console.log(10==10);
-console.log("10" == 10);
-console.log("hi" == "hi");
-console.log("10" === "10");
-console.log("10" === 10);
-console.log(10 != 10);
-console.log("10" !== 10);
+// console.log(10==10);
+// console.log("10" == 10);
+// console.log("hi" == "hi");
+// console.log("10" === "10");
+// console.log("10" === 10);
+// console.log(10 != 10);
+// console.log("10" !== 10);
 
-//logical operators
+// //logical operators
 
 
 
